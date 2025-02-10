0b342cf7d3e8997fda5462400f1982e5b2a9bbb6
bhargava julaganti <bhargavjulaganti@gmail.com>
2025-02-09 20:05:09 -0500

    replacing inroduction image on home page

--- src/enums/asset-paths.enum.ts
+++ src/enums/asset-paths.enum.ts
@@ -1,6 +1,6 @@
 export enum AssetPaths {
     RESUME_IT = "Dhruvil_Rathod.pdf",
-    FEELING_PROUD_SVG = "assets/illustrations/FeelingProud.svg",
+    FEELING_PROUD_SVG = "assets/illustrations/helloIntroduction.svg",
     DIGITAL_SOLUTIONS_SVG = "assets/illustrations/DigitalSolutions.svg",
     FULL_STACK_DEVELOPMENT_SVG = "assets/illustrations/FullStackDevelopment.svg",
     CLOUD_INFRASTRUCTURE_SVG = "assets/illustrations/CouldInfrastructure.svg",
